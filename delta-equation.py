8fc8326b689c8f7d5ae3ae3515a0a43c1adcd165
Py_elpadre <mathis.padre@gmail.com>
2020-05-08 17:20:29 +0400

    Initial commit

---
+++ delta-equation.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+print("Bonjour ! Nous allons resoudre une equation du second degré")
+
+a = int(input("Quelle est la valeur de a ?"))
+b = float(input("Quelle est la valeur de b ?"))
+c = float(input("Quelle est la valeur de c ?"))
+
+delta = b**2 - 4*a*c
+
+if delta > 0:
+    x1 = (-sqrt(delta)-b)/(2*a)
+    x2 = (sqrt(delta)-b)/(2*a)
+    print("Les solutions de l'équation sont", x1, "et", x2)
+
+elif delta < 0:
+    print("L'équation n'a pas de solution dans R")
+
+elif delta == 0:
+    x1 = x2 = -b / (2*a)
+    print("Léquation a une solution double :", x1)
+
+
+
+
+
+
+
+
+
+
+
